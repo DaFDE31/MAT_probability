42832cbfe6a0d51ee6362e50835fdcf0a26073c2
danielson <dadjocy21@students.desu.edu>
2023-10-11 14:17:37 -0400

    DONE

--- Cardinality.m
+++ Cardinality.m
@@ -1,4 +1,4 @@
-function [] = Testdrive(x)
+function [] = Cardinality(x)
     text = "[";
     for looper = 1:1:length(x)-1
         text = text + x(looper)+", ";
@@ -6,4 +6,9 @@ function [] = Testdrive(x)
     text = text + x(length(x))+"]: Cardinality: "+ length(x); 
     disp(text);
     disp(2^length(x)+" possible events");
+    disp(" ")
+    disp("Number of events for each cardinality")
+    for card = 0:1:length(x)
+        disp("|"+card+"|: "+ CnR(length(x), card));
+    end
 end
